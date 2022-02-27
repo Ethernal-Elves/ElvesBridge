b1cd5a19e19d0e7a6a2f5d0e98a8e7a9c2b9bc32
africabusinessnews <71972487+africabusinessnews@users.noreply.github.com>
2022-02-27 22:05:15 +0400

    Update App.js

--- src/App.js
+++ src/App.js
@@ -5,9 +5,6 @@ import {
   Route
 } from "react-router-dom";
 import MainLayout from './templates/MainLayout';
-import Home from './views/home';
-import { getCurrentWalletConnected } from './utils/interact'
-import { useState, useEffect } from 'react';
 import Transfers from './views/Transfers';
 
 require('dotenv').config();
@@ -15,25 +12,7 @@ require('dotenv').config();
 
 function App() {
 
-const [flip, setFlip] = useState(false)  
 
-  let dev1 = process.env.REACT_APP_DEV1
-  let dev2 = process.env.REACT_APP_DEV2
-  let dev3 = process.env.REACT_APP_DEV3
-
-
-  let adminWallet = [dev1.toLowerCase(), dev2.toLowerCase(), dev3.toLowerCase()]
-
-  useEffect(async() => {
-
-
-    const {address} = await getCurrentWalletConnected()
-
-    if(adminWallet.includes(address.toLowerCase())){
-      setFlip(true)
-    }
-
-  }, [])
   return (
       <div className="App">
         <Router>
